47ab2509d4db069fe301a4a79044bbba0b995f0a
Steven Omeis <2369519O@student.gla.ac.uk>
2018-01-24 16:26:43 +0000

    added up to chapter 5

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,8 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     #return a rendered response to the client
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    
+    return render(request, 'rango/about.html', context=context_dict)
