6fc228cb88ba0c52930d8328ef6db5c4ff37f5be
Steven Omeis <2369519O@student.gla.ac.uk>
2018-01-26 05:48:06 +0000

    added work up to 5.8 but not including the exercise

---
+++ populate_rango.py
@@ -0,0 +1,58 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
+
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+    python_pages = [
+    {"title":"Official Python Tutorial",
+    "url":"http://docs.python.org/2/tutorial/"},
+    {"title":"How to Think like a Computer Sicentist",
+    "url":"http://www.greenteapress.com/thinkpython/"},
+    {"title":"Learn Python in 10 Minutes",
+    "url":"http://www.korokithakis.net/tutorials/python/"}]
+
+    django_pages = [
+    {"title":"Official Django Tutorial",
+    "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+    {"title":"Django Rocks",
+    "url":"http://www.djangorocks.com/"},
+    {"title":"How to Tango with Django",
+    "url":"http://www.tangowithdjango.com/"}]
+
+    other_pages = [
+    {"title":"Bottle",
+    "url":"http://bottleypy.org/docs/dev/"},
+    {"title":"Flask",
+    "url":"http://flask.pocoo.org"}]
+
+    cats = {"Python": {"pages": python_pages}, #views and likes for 5.8   128,64
+            "Django": {"pages": django_pages}, #64 ,32
+            "Other Frameworks": {"pages": other_pages}} #32 ,16
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat) #add the proper parameters for exercise 5.8 cats[cat]["lkes"] ??????
+        for p in cat_data["pages"]:
+            add_page(c, p["title"], p["url"])
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name): #add views, likes for exercise 5.8
+    c = Category.objects.get_or_create(name=name)[0] #add views and likes for exercise 5.8
+    c.save()
+    return c
+
+if __name__ == '__main__':
+        print("Starting Rango population script...")
+        populate()
